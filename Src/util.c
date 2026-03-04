92c07006ce39a6925516513c30e3239e080cd416
萌新钢铁侠 <2631707047@qq.com>
2026-03-04 16:33:34 +0800

    打开其他声音模块

--- Src/util.c
+++ Src/util.c
@@ -417,32 +417,32 @@ void UART_DisableRxErrors(UART_HandleTypeDef *huart)
 /* =========================== General Functions =========================== */
 
 void poweronMelody(void) {
-    // buzzerCount = 0;  // prevent interraction with beep counter
-    // for (int i = 8; i >= 0; i--) {
-    //   buzzerFreq = (uint8_t)i;
-    //   HAL_Delay(100);
-    // }
-    // buzzerFreq = 0;
+    buzzerCount = 0;  // prevent interraction with beep counter
+    for (int i = 8; i >= 0; i--) {
+      buzzerFreq = (uint8_t)i;
+      HAL_Delay(100);
+    }
+    buzzerFreq = 0;
 }
 
 void beepCount(uint8_t cnt, uint8_t freq, uint8_t pattern) {
-    // buzzerCount   = cnt;
-    // buzzerFreq    = freq;
-    // buzzerPattern = pattern;
+    buzzerCount   = cnt;
+    buzzerFreq    = freq;
+    buzzerPattern = pattern;
 }
 
 void beepLong(uint8_t freq) {
-    // buzzerCount = 0;  // prevent interraction with beep counter
-    // buzzerFreq = freq;
-    // HAL_Delay(500);
-    // buzzerFreq = 0;
+    buzzerCount = 0;  // prevent interraction with beep counter
+    buzzerFreq = freq;
+    HAL_Delay(500);
+    buzzerFreq = 0;
 }
 
 void beepShort(uint8_t freq) {
-    // buzzerCount = 0;  // prevent interraction with beep counter
-    // buzzerFreq = freq;
-    // HAL_Delay(100);
-    // buzzerFreq = 0;
+    buzzerCount = 0;  // prevent interraction with beep counter
+    buzzerFreq = freq;
+    HAL_Delay(100);
+    buzzerFreq = 0;
 }
 
 void beepShortMany(uint8_t cnt, int8_t dir) {
